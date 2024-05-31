b3d6f9420686286114c135f611c44324134946b7
surita <suritabtc@gmail.com>
2024-05-31 11:19:08 -0500

    .

--- front/src/components/VideoPlayer/VidePlayer.tsx
+++ front/src/components/VideoPlayer/VidePlayer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './ChatVideoPlayer.module.css'; // Crea un archivo CSS si necesitas estilos
+import styles from './ChatVideoPlayer.module.css';
 
 interface ChatVideoPlayerProps {
     src: string;
@@ -10,7 +10,6 @@ const ChatVideoPlayer: React.FC<ChatVideoPlayerProps> = ({ src }) => {
         <div className={styles.videoContainer}>
             <video className={styles.videoPlayer} controls>
                 <source src={src} type="video/mp4" />
-                Your browser does not support the video tag.
             </video>
         </div>
     );
